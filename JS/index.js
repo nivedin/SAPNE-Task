504f27ea030c903c651d266d90f46fe5bb697f7e
Nivedin <nivedinp@gmail.com>
2020-05-11 14:03:59 +0530

    Completed Task

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+const images = document.querySelectorAll('.anim');
+
+      obeserver = new IntersectionObserver((entries) =>{
+          entries.forEach(entry =>{
+              if(entry.intersectionRatio > 0){
+                entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
+              }
+              else{
+                entry.target.style.animation = 'none';
+              }
+        });
+      });
+
+
+      images.forEach(image =>{
+        obeserver.observe(image);
+      })
